451838f2a3d446c29149e2943b517f0539fdf957
wy <hustwatson@gmail.com>
2021-11-15 19:07:04 +0800

    add serialize

---
+++ internal/order/order_test.go
@@ -0,0 +1,56 @@
+package order
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/rockiecn/check/internal/utils"
+)
+
+func TestDB(t *testing.T) {
+	mgr := NewMgr()
+	odr := NewOdr(1,
+		common.HexToAddress("0xb213d01542d129806d664248a380db8b12059061"),
+		common.HexToAddress("0x5B38Da6a701c568545dCfcB03FcB875f56beddC4"),
+		common.HexToAddress("0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2"),
+		utils.String2BigInt("300000000000000000"), // order value: 0.3 eth
+		time.Now(),
+		"jack",
+		"123123123",
+		"asdf@asdf.com",
+		0,
+	)
+	if odr == nil {
+		t.Fatal("create order failed")
+	}
+
+	fmt.Printf("original order:\n%v\n", odr)
+
+	// marshal order
+	buf, err := mgr.MarshOdr(odr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// put into db
+	err = mgr.WriteDB(1, buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// read from db
+	newBuf, err := mgr.ReadDB(1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// unmarshal order
+	newOdr, err := mgr.UnMarshOdr(newBuf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Printf("unmarshaled order read from db:\n%v\n", newOdr)
+}
